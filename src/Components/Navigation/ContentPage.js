fa879d4a835cdc79c3c9a93c6b552d53cab8b589
sangwa2 <sangwa223@outlook.com>
2023-01-27 22:26:03 +0200

    added reviewhome 2

--- src/Components/Navigation/ContentPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ContentPage() {
-  return (
-    <div>This is the ContentPage</div>
-  )
-}
-
-export default ContentPage
--- src/Components/Navigation/TestProps.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MyProps= ()=> {
-  return (
-    <div> <h2> Called via Link and browserRouter </h2></div>
-  )
-}
-
-export default MyProps
