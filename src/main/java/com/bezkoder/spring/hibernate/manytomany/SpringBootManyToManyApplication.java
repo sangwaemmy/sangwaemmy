cc473fa2185ecb317558de1ea9b9f2e77f3129f7
sangwa2 <sangwa223@outlook.com>
2023-02-04 10:48:17 +0200

    Thee are the oneToMany example with related tables. been pushed from home Sturday

--- src/main/java/com/bezkoder/spring/hibernate/manytomany/SpringBootManyToManyApplication.java
+++ src/main/java/com/bezkoder/spring/hibernate/manytomany/SpringBootManyToManyApplication.java
@@ -1,13 +1,13 @@
-package com.jpaonetoonedemo;
+package com.bezkoder.spring.hibernate.manytomany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaOneToOneDemoApplication {
+public class SpringBootManyToManyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpaOneToOneDemoApplication.class, args);
+		SpringApplication.run(SpringBootManyToManyApplication.class, args);
 	}
 
 }
